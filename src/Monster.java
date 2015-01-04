91a1c25750a2ca84d1a50b06064d50696c91542e
Nico Weil <nico.weil@web.de>
2015-01-04 19:56:43 +0100

    Added javadoc comments; minor fixes.

--- src/Monster.java
+++ src/Monster.java
@@ -1,157 +1,165 @@
-
 public class Monster {
-	
-	private int hp;
-	private int maxHP;
-	private int atk;
-	private double hitChance;
-	private boolean frozen;
-	private int specialAbilityDuration;
-	private boolean decreasedHitChance;
-	private double decreasedHitPoints;
 
-	
-	public Monster(int maxH, int a, double y) {
-		
-		this.hp = maxH;
-		this.maxHP = maxH;
-		this.atk = a;
-		this.hitChance = y;
-		this.frozen = false;
-		this.specialAbilityDuration = 0;
-		this.decreasedHitChance = false;
-		this.decreasedHitPoints = 0.0;
+    private int hp;
+    private int maxHP;
+    private int atk;
+    private double hitChance;
+    private boolean frozen;
+    private int specialAbilityDuration;
+    private double decreasedHitPoints;
 
-	}
-	
-	public Monster() {
-		
-		this.hp = 200;
-		this.maxHP = 200;
-		this.atk = 10;
-		this.hitChance = 0.9;
-		this.frozen = false;
-		this.specialAbilityDuration = 0;
-		this.decreasedHitChance = false;
-		this.decreasedHitPoints = 0.0;
+    public Monster(int maxH, int a, double y) {
 
-	
-	}
-	
-	/**
-	 * Methode isDefeated ueberprueft, ob Monster besiegt ist
-	 */
-	
-	public boolean isDefeated() {
-		if(this.hp <= 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Methode takeDamage fügt dem Monster den durch damage übergebenen Schaden zu.
-	 */
-	
-	public void takeDamage(int damage) {
-		this.hp -= damage;
-		if(this.hp <= 0) {
-			this.hp = 0;
-		}
-	}
-	
-	/**
-	 * Methode attack(Player player) fügt dem uebergebenen Objekt Schaden zu.
-	 */
-	
-	public int attack(Player player) {
-		double zufallsZahlVergleich = Math.random();
-		if(zufallsZahlVergleich > (this.hitChance - this.decreasedHitPoints)) {
-			return -1;
-		} else {
-			double zufallsZahl = Math.random() + 1;
-			player.takeDamage((int) (this.atk * zufallsZahl));
-			return player.getHp();
-		}
-	}
-	
-	/**
-	 * Methode toString() gibt HP, ATK, Items zurueck.
-	 */
-	
-	public String toString() {
-		return "Monster -- HP " + this.hp + " -- ATK " + this.atk + " -- Decreased Hit Chance " + this.decreasedHitPoints;
-	}
-	
-	/**
-	 * Methode freeze() "friert" das Monster ein, sein Angriff auf den Spieler wird also uebersprungen.
-	 */
-	
-	public void freeze() {
-		this.specialAbilityDuration = 3;
-		this.frozen = true;
-	}
-	
-	/**
-	 * Methode isFrozen() gibt einen booleanschen Wert zurueck, ob das Monster "eingefroren" ist oder nicht und verringert die noch verbleibende Rundenzeit um eins.
-	 */
-	
-	public boolean isFrozen() {
-			return this.frozen;
-	}
-	
-	/**
-	 * Methode roundFinishes() senkt die specialAbilityDuration (falls >0) um 1 und setzt this.frozen auf 0, sobald specialAbilityDuration=0.
-	 * Außerdem werden die decreasedHitPints auf 0 gesetzt falls diese > 0.
-	 */
-	
-	public void roundFinished() {
-		this.specialAbilityDuration --;
-		if(this.specialAbilityDuration <= 0) {
-			this.specialAbilityDuration = 0;
-			if(this.frozen) {
-				this.frozen = false;
-				if(this.hp > 0) {
-					System.out.println("Das Monster taut wieder auf!");
-				}
-			}
-			if(this.decreasedHitPoints != 0 && this.hp > 0) {
-				this.decreasedHitPoints = 0;
-				System.out.println("Dem Monster ist nicht mehr schwindelig!");
-			}
-		}
-	}
-	
-	public void hasDecreasedHitChance(double x) {
-		this.specialAbilityDuration = 3;
-		this.decreasedHitPoints = x;
-	}
-	
-	/**
-	 * Methode isAbilityActive() ueberprueft ob eine spezielle Faehigkeit noch aktiv ist.
-	 */
-	
-	public boolean isAbilityActive() {
-		if(this.specialAbilityDuration > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public double getDecreasedHitPoints() {
-		return this.decreasedHitPoints;
-	}
-	
-	/**
-	 * Methode getHP() gibt Lebenspunkte zurueck.
-	 */
-	
-	public int getHp() {
-		return this.hp;
-		
-	}
+        this.hp = maxH;
+        this.maxHP = maxH;
+        this.atk = a;
+        this.hitChance = y;
+        this.frozen = false;
+        this.specialAbilityDuration = 0;
+        this.decreasedHitPoints = 0.0;
+
+    }
+
+    public Monster() {
+
+        this.hp = 200;
+        this.maxHP = 200;
+        this.atk = 10;
+        this.hitChance = 0.9;
+        this.frozen = false;
+        this.specialAbilityDuration = 0;
+        this.decreasedHitPoints = 0.0;
+
+    }
+
+    /**
+     * Methode isDefeated ueberprueft, ob Monster besiegt ist.
+     */
+
+    public boolean isDefeated() {
+        if (this.hp <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Methode takeDamage fügt dem Monster den durch damage übergebenen Schaden
+     * zu.
+     */
+
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
+    }
+
+    /**
+     * Methode attack(Player player) fügt dem uebergebenen Objekt Schaden zu.
+     */
+
+    public int attack(Player player) {
+        double zufallsZahlVergleich = Math.random();
+        if (zufallsZahlVergleich > (this.hitChance - this.decreasedHitPoints)) {
+            return -1;
+        } else {
+            double zufallsZahl = Math.random() + 1;
+            player.takeDamage((int) (this.atk * zufallsZahl));
+            return player.getHp();
+        }
+    }
+
+    /**
+     * Methode toString() gibt HP, ATK, Items zurueck.
+     */
+
+    public String toString() {
+        return "Monster -- HP " + this.hp + " -- ATK " + this.atk + " -- Decreased Hit Chance " + this.decreasedHitPoints;
+    }
+
+    /**
+     * Methode freeze() "friert" das Monster ein, sein Angriff auf den Spieler
+     * wird also uebersprungen.
+     */
+
+    public void freeze() {
+        this.specialAbilityDuration = 3;
+        this.frozen = true;
+    }
+
+    /**
+     * Methode isFrozen() gibt einen booleanschen Wert zurueck, ob das Monster
+     * "eingefroren" ist oder nicht.
+     */
+
+    public boolean isFrozen() {
+        return this.frozen;
+    }
+
+    /**
+     * Methode roundFinishes() senkt die specialAbilityDuration (falls >0) um 1
+     * und setzt this.frozen auf 0, sobald specialAbilityDuration=0. Außerdem
+     * werden die decreasedHitPints auf 0 gesetzt falls diese > 0.
+     */
+
+    public void roundFinished() {
+        this.specialAbilityDuration--;
+        if (this.specialAbilityDuration <= 0) {
+            this.specialAbilityDuration = 0;
+            if (this.frozen) {
+                this.frozen = false;
+                if (this.hp > 0) {
+                    System.out.println("Das Monster taut wieder auf!");
+                }
+            }
+            if (this.decreasedHitPoints != 0 && this.hp > 0) {
+                this.decreasedHitPoints = 0;
+                System.out.println("Dem Monster ist nicht mehr schwindelig!");
+            }
+        }
+    }
+
+    /**
+     * Methode hasDecreasedHitChance(doubel x) setzt specialAbilityDuration auf
+     * 3 und decreasedHitPints (welche im Angriff abgezogen werden) auf den in
+     * den Klammern uebergebenen Wert.
+     */
+
+    public void hasDecreasedHitChance(double x) {
+        this.specialAbilityDuration = 3;
+        this.decreasedHitPoints = x;
+    }
+
+    /**
+     * Methode isAbilityActive() ueberprueft ob eine spezielle Faehigkeit noch
+     * aktiv ist.
+     */
+
+    public boolean isAbilityActive() {
+        if (this.specialAbilityDuration > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Methode getDecreasedHitPoints() gibt decreasedHitPoints zurueck.
+     */
+
+    public double getDecreasedHitPoints() {
+        return this.decreasedHitPoints;
+    }
+
+    /**
+     * Methode getHP() gibt Lebenspunkte zurueck.
+     */
+
+    public int getHp() {
+        return this.hp;
+
+    }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,203 +1,226 @@
-
 public class Player {
-	
-	private int hp;
-	private int maxHP;
-	private int atk;
-	private int healingPower;
-	private int remainingItemUses;
-	private double hitChance;
-	private int ap;
-	private int maxAp;
-	private int apRegen;
-	private int bonusAtk;
-	private int specialAbilityDuration;
-	
-	public Player( int maxH, int a, int x, int r, double y) {
-		
-		this.hp = maxH;
-		this.maxHP = maxH;
-		this.atk = a;
-		this.healingPower = x;
-		this.remainingItemUses = r;
-		this.hitChance = y;
-		
-	}
-	
-	public Player(int maxH, int a, int w, int r, double x, int maxY, int y, int z) {
-		
-		this.hp = maxH;
-		this.maxHP = maxH;
-		this.atk = a;
-		this.healingPower = w;
-		this.remainingItemUses = r;
-		this.hitChance = x;
-		this.maxAp = maxY;
-		this.ap = y;
-		this.apRegen = z;
-		this.bonusAtk= 0;
-		this.specialAbilityDuration = 0;
-	}
-	
-	public Player() {
-		
-		this.maxHP = 100;
-		this.hp = this.maxHP;
-		this.atk = 15;
-		this.healingPower = 40;
-		this.remainingItemUses = 3;
-		this.hitChance = 0.7;
-	}
-	
-	
-	/**
-	 * Methode isDefeated() kontrolloiert, ob Spieler besiegt ist.
-	 */
-	
-	public boolean isDefeated() {
-		if(this.hp <= 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	/**
-	 * Methode takeDamage(int damage) fügt dem Spieler den ducrh damage angegebenen Schaden zu.
-	 */
-	
-	public void takeDamage(int damage) {
-		this.hp -= damage;
-		if(this.hp <= 0) {
-			this.hp = 0;
-		}
-	}
-	
-	/**
-	 * Methode attackMonster(Monster monster) fuegt dem uebergebenen Monsterobjekt Schaden zu.
-	 * Methode hat zwei Angriffsarten: Wenn Option "Powerpille" gewaehlt wird, wird der Angriff mit durch takePowerPill() generierte BonusATK zusaetzlich zu den ATK durchgefuehrt. Wenn nicht, wird der Angriff mit den normalen ATK durchgefuehrt. 
-	 */
-	
-	public int attackMonster(Monster monster) {
-		double zufallsZahlVergleich = Math.random();
-		if(zufallsZahlVergleich > this.hitChance) {
-			return -1;
-		} else {
-				double zufallsZahl = Math.random() + 1;
-				monster.takeDamage((int) ((this.atk + this.bonusAtk) * zufallsZahl));
-				return monster.getHp();
-			}
-	}
-	
-	/**
-	 * Methode getRemainingItemUses() gibt an, wie oft der Spieler noch einen Heiltrank einsetzen kann.
-	 */
-	
-	public int getRemainingItemUses() {
-		return this.remainingItemUses;
-	}
-	
-	/**
-	 * Die Methode heal() setzt einen Heiltrank ein (falls noch übrig).
-	 */
-	
-	public boolean heal() {
-		if(this.remainingItemUses > 0) {
-			this.hp = this.hp + this.healingPower;
-			this.remainingItemUses = this.remainingItemUses -1;
-			if(this.hp > this.maxHP) {
-				this.hp = this.maxHP;
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean fullHp() {
-		return (this.hp == this.maxHP);
-	}
-	
-	/**
-	 * Methode toString() gibt HP, ATK, AP, Items zurueck.
-	 */
-	
-	public String toString() {
-		return "Spieler -- HP " + this.hp  + " -- ATK " + this.atk + " -- " + " BonusATK " + this.bonusAtk + " -- AP " + this.ap  + " -- " + this.remainingItemUses + " Heiltraenke";
-		
-	}
-	
-	/**
-	 * Methode regenerateAp() regeneriert die AP des Spielers bis zum maxAp.
-	 */
-	
-	public int regenerateAp() {
-		int preAp = this.ap;
-		this.ap = this.ap + this.apRegen;
-		if(this.ap > this.maxAp) {
-			this.ap = this.maxAp;
-		}
-		return this.ap - preAp;
-	}
-	
-	/** 
-	 * Methode takePowerPill() generiert Bonus-ATK, sodass auf die ATK im Angriff nicht nur 0, sondern ein Anteil der ATK addiert werden.
-	 */
-	
-	public void takePowerPill() {
-		this.specialAbilityDuration = 3;
-		double multiplier = (Math.random() * 0.9 + 0.1);
-		this.bonusAtk = (int) (this.ap * multiplier);
-	}
-	
-	/**
-	 * Methode decreaseAp(int apCosts) verringert die AP des Spielers um den in Klammern angegebenen Wert.
-	 */
-	
-	public void decreaseAp(int apCosts) {
-		this.ap = this.ap - apCosts;
-	}
-	
-	/**
-	 * Methode roundFinished() verringert specialAbilityDuration um 1. Sie verhindert specialAbilityDuration<0 und setzt bonusATK=0, sobald specialAbilityDuration=0.
-	 */
-	
-	public void roundFinished() {
-		this.specialAbilityDuration --;
-		if(this.specialAbilityDuration <= 0) {
-			this.specialAbilityDuration = 0;
-			if(this.bonusAtk > 0 && this.hp >0) {
-				this.bonusAtk = 0;
-				System.out.println("Die Wirkung der Powerpille laesst nach!");
-			}
-		}
-	}
-	
-	/**
-	 * Methode isAbilityActive() ueberprueft, ob eine spezielle Faehigkeit noch aktiv ist.
-	 */
-	
-	public boolean isAbilityActive() {
-		return (this.specialAbilityDuration > 0);
-	}
-	
-	public boolean enoughAp(int apNeeded) {
-		return (this.ap >= apNeeded);
-	}
-	
-	public int getBonusAtk() {
-		return this.bonusAtk;
-	}
-	
-	/**
-	 * Methode getHP() gibt HP zurueck.
-	 */
-	
-	public int getHp() {
-		return this.hp;
-	}
-	
-	
+
+    private int hp;
+    private int maxHP;
+    private int atk;
+    private int healingPower;
+    private int remainingItemUses;
+    private double hitChance;
+    private int ap;
+    private int maxAp;
+    private int apRegen;
+    private int bonusAtk;
+    private int specialAbilityDuration;
+
+    public Player(int maxH, int a, int x, int r, double y) {
+
+        this.hp = maxH;
+        this.maxHP = maxH;
+        this.atk = a;
+        this.healingPower = x;
+        this.remainingItemUses = r;
+        this.hitChance = y;
+
+    }
+
+    public Player(int maxH, int a, int w, int r, double x, int maxY, int y, int z) {
+
+        this.hp = maxH;
+        this.maxHP = maxH;
+        this.atk = a;
+        this.healingPower = w;
+        this.remainingItemUses = r;
+        this.hitChance = x;
+        this.maxAp = maxY;
+        this.ap = y;
+        this.apRegen = z;
+        this.bonusAtk = 0;
+        this.specialAbilityDuration = 0;
+    }
+
+    public Player() {
+
+        this.maxHP = 100;
+        this.hp = this.maxHP;
+        this.atk = 15;
+        this.healingPower = 40;
+        this.remainingItemUses = 3;
+        this.hitChance = 0.7;
+    }
+
+    /**
+     * Methode isDefeated() kontrolloiert, ob Spieler besiegt ist.
+     */
+
+    public boolean isDefeated() {
+        if (this.hp <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Methode takeDamage(int damage) fügt dem Spieler den ducrh damage
+     * angegebenen Schaden zu.
+     */
+
+    public void takeDamage(int damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
+    }
+
+    /**
+     * Methode attackMonster(Monster monster) fuegt dem uebergebenen
+     * Monsterobjekt Schaden zu. Methode hat zwei Angriffsarten: Wenn Option
+     * "Powerpille" gewaehlt wird, wird der Angriff mit durch takePowerPill()
+     * generierte BonusATK zusaetzlich zu den ATK durchgefuehrt. Wenn nicht,
+     * wird der Angriff mit den normalen ATK durchgefuehrt. Methode gibt -1
+     * zurueck falls der Angriff nicht trifft.
+     */
+
+    public int attackMonster(Monster monster) {
+        double zufallsZahlVergleich = Math.random();
+        if (zufallsZahlVergleich > this.hitChance) {
+            return -1;
+        } else {
+            double zufallsZahl = Math.random() + 1;
+            monster.takeDamage((int) ((this.atk + this.bonusAtk) * zufallsZahl));
+            return monster.getHp();
+        }
+    }
+
+    /**
+     * Methode getRemainingItemUses() gibt an, wie oft der Spieler noch einen
+     * Heiltrank einsetzen kann.
+     */
+
+    public int getRemainingItemUses() {
+        return this.remainingItemUses;
+    }
+
+    /**
+     * Die Methode heal() setzt einen Heiltrank ein (falls noch uebrig). Methode
+     * gibt true zurueck, wenn die Heilung erfolgreich war, und false, falls die
+     * Heilung nicht erfolgreich war.
+     */
+
+    public boolean heal() {
+        if (this.remainingItemUses > 0) {
+            this.hp = this.hp + this.healingPower;
+            this.remainingItemUses = this.remainingItemUses - 1;
+            if (this.hp > this.maxHP) {
+                this.hp = this.maxHP;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Methode fullHp() gibt booleanschen Wert zurueck ob die HP = maxHP.
+     */
+
+    public boolean fullHp() {
+        return (this.hp == this.maxHP);
+    }
+
+    /**
+     * Methode toString() gibt HP, ATK, BonusATK, AP, Items zurueck.
+     */
+
+    public String toString() {
+        return "Spieler -- HP " + this.hp + " -- ATK " + this.atk + " -- " + " BonusATK " + this.bonusAtk + " -- AP " + this.ap + " -- " + this.remainingItemUses + " Heiltraenke";
+
+    }
+
+    /**
+     * Methode regenerateAp() regeneriert die AP des Spielers bis zum maxAp und
+     * gibt die Differenz zurueck.
+     */
+
+    public int regenerateAp() {
+        int preAp = this.ap;
+        this.ap = this.ap + this.apRegen;
+        if (this.ap > this.maxAp) {
+            this.ap = this.maxAp;
+        }
+        return this.ap - preAp;
+    }
+
+    /**
+     * Methode takePowerPill() generiert Bonus-ATK, sodass auf die ATK im
+     * Angriff nicht nur 0, sondern ein Anteil der ATK addiert werden.
+     */
+
+    public void takePowerPill() {
+        this.specialAbilityDuration = 3;
+        double multiplier = (Math.random() * 0.9 + 0.1);
+        this.bonusAtk = (int) (this.ap * multiplier);
+    }
+
+    /**
+     * Methode decreaseAp(int apCosts) verringert die AP des Spielers um den in
+     * Klammern angegebenen Wert.
+     */
+
+    public void decreaseAp(int apCosts) {
+        this.ap = this.ap - apCosts;
+    }
+
+    /**
+     * Methode roundFinished() verringert specialAbilityDuration um 1. Sie
+     * verhindert specialAbilityDuration<0 und setzt bonusATK=0, sobald
+     * specialAbilityDuration=0. Methode wird benoetigt, um Objekten
+     * "mitzuteilen", dass eine Runde zuende ist.
+     */
+
+    public void roundFinished() {
+        this.specialAbilityDuration--;
+        if (this.specialAbilityDuration <= 0) {
+            this.specialAbilityDuration = 0;
+            if (this.bonusAtk > 0 && this.hp > 0) {
+                this.bonusAtk = 0;
+                System.out.println("Die Wirkung der Powerpille laesst nach!");
+            }
+        }
+    }
+
+    /**
+     * Methode isAbilityActive() ueberprueft, ob eine spezielle Faehigkeit noch
+     * aktiv ist.
+     */
+
+    public boolean isAbilityActive() {
+        return (this.specialAbilityDuration > 0);
+    }
+
+    /**
+     * Methode enoughAp() gibt boolenschen Wert zurueck, ob so viele AP
+     * vorhanden sind, wie in den Klammern angegeben.
+     */
+
+    public boolean enoughAp(int apNeeded) {
+        return (this.ap >= apNeeded);
+    }
+
+    /**
+     * Methode getBonusAtk() gibt die BonusATK zurueck.
+     */
+
+    public int getBonusAtk() {
+        return this.bonusAtk;
+    }
+
+    /**
+     * Methode getHP() gibt HP zurueck.
+     */
+
+    public int getHp() {
+        return this.hp;
+    }
 
 }
