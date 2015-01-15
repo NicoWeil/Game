b1c029a7da6e7141a561cbf4b2a93da2f837f044
Nico Weil <nico.weil@web.de>
2015-01-15 19:49:34 +0100

    ...

--- src/Level.java
+++ src/Level.java
@@ -10,8 +10,33 @@ public class Level {
     mapData[0] [0] = 'x';
     mapData[0] [1] =
     mapData[0] [2] =    
-    mapData[0] [2]
-    mapData[0] [2]
+    mapData[0] [3] =
+    mapData[0] [4] =
+    mapData[1] [0] =
+    mapData[1] [1] =
+    mapData[1] [2] =
+    mapData[1] [3] =
+    mapData[1] [4] =
+    mapData[2] [0] =
+    mapData[2] [1] =
+    mapData[2] [2] =
+    mapData[2] [3] =
+    mapData[2] [4] =
+    mapData[3] [0] =
+    mapData[3] [1] =
+    mapData[3] [2] =
+    mapData[3] [3] =
+    mapData[3] [4] =
+    mapData[4] [0] =
+    mapData[4] [1] =
+    mapData[4] [2] =
+    mapData[4] [3] =
+    mapData[4] [4] =
+    mapData[5] [0] =
+    mapData[5] [1] =
+    mapData[5] [2] =
+    mapData[5] [3] =
+    mapData[5] [4] =
             
     }
         
